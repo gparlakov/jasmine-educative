d19bb3a20f5c250289490e618b4d85277850d961
gparlakov <gparlakov@gmail.com>
2021-08-21 14:15:58 +0300

    feat(testing-date): add exercise and few tests

---
+++ 2.10.testing-dates-exercise/spec/days-until.spec.mjs
@@ -0,0 +1,17 @@
+
+import { daysUntil } from '/__src__/days-until.mjs';
+
+describe('daysUntil', () => {
+  it('should return 5 for a date 5 days in the future', () => {
+    pending('replace with logic for 5 days in the future');
+  });
+
+  it('should return 5 for a date 5 and 1/4 days in the future', () => {
+    pending('replace with logic for 5 and 1/4 days in the future');
+
+  });
+
+  it('should return 5 for a date 4 and 3/4 days in the future', () => {
+    pending('replace with logic for 4 and 3/4 days in the future');
+  });
+});
--- 2.10.testing-dates/spec/days-ago.spec.mjs
+++ 2.10.testing-dates/spec/days-ago.spec.mjs
@@ -15,4 +15,33 @@ describe('daysAgo', () => {
 
     clock.uninstall();
   });
+
+  it('should return 7 for a date 6 and 3/4 days ago', () => {
+    // arrange
+    const clock = jasmine.clock()
+    clock.install().mockDate(new Date(2021, 8, 10));
+
+    // act
+    const ago = daysAgo(new Date(2021, 8, 3, 6, 0, 0));
+
+    // assert
+    expect(ago).toBe(7);
+
+    clock.uninstall();
+  });
+
+  it('should return 7 for a date 7 and 1/4 days ago', () => {
+    // arrange
+    const clock = jasmine.clock()
+    clock.install().mockDate(new Date(2021, 8, 10));
+
+    // act
+    const ago = daysAgo(new Date(2021, 8, 2, 18, 0, 0));
+
+    // assert
+    expect(ago).toBe(7);
+
+    clock.uninstall();
+  });
+
 });
