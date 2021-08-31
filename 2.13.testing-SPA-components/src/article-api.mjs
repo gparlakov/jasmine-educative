e326f7a23c6c5e7faab1126cf0c5fcf2fd9cbadc
Georgi Parlakov <gparlakov@gmail.com>
2021-08-31 09:18:18 +0300

    feat(testing-spa-component): init

---
+++ 2.13.testing-SPA-components/src/article-api.mjs
@@ -0,0 +1,10 @@
+
+export class ArticleAPI {
+    delete(id) {
+      // make a DELETE /api/article/:id request (XMLHttpRequest/fetch/axios)
+    }
+
+    create(title, content) {
+      // make a POST /api/article request (XMLHttpRequest/fetch/axios)
+    }
+  }
---
+++ 2.13.testing-SPA-components/src/articles-component.js
@@ -0,0 +1,9 @@
+export class ArticlesComponent {
+    afterComponentInit() {
+
+    }
+
+    componentWillGetDestroyed() {
+
+    }
+}
