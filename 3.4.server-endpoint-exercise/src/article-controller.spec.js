9f6deab7f1d68fff9311757c9fc65767d659d857
Georgi Parlakov <gparlakov@gmail.com>
2021-09-29 20:15:54 +0300

    feat(server-endpoint): finish exercise 1

--- 3.4.server-endpoint-exercise/src/article-controller.spec.js
+++ 3.4.server-endpoint-exercise/src/article-controller.spec.js
@@ -40,50 +40,30 @@ describe('ArticleApiController', () => {
   describe('when delete called', () => {
     it('with an id and matching etag to version should delete the article', async () => {
       // arrange
-      articleCollection.findOne.and.returnValue(Promise.resolve({ id: 1, version: '2' }));
-      articleCollection.delete.and.returnValue(Promise.resolve({ id: 1, version: '2' }));
+      pending('add test logic here - hint - use `await expectAsync` ')
       // act
-      const a = await articleController.delete(1, '2');
       // assert
-      expect(articleCollection.delete).toHaveBeenCalledOnceWith({ id: 1 });
     });
 
     it('with an id and mismatching etag to version should not delete the article and reject with a status object', async () => {
       // arrange
-      articleCollection.findOne.and.returnValue(Promise.resolve({ id: 1, version: '3' }));
-      articleCollection.delete.and.returnValue(Promise.resolve({ id: 1, version: '2' }));
+      pending('add test logic here - hint - use `await expectAsync` ')
       // act
-      const deletePromise = articleController.delete(1, '2');
       // assert
-      await expectAsync(deletePromise).toBeRejectedWith({
-        status: 'version mismatch',
-        message: 'Article with id "1" seems to have been updated and client and server versions do not match.',
-      });
-      expect(articleCollection.delete).not.toHaveBeenCalled();
     });
 
     it('and get article fails it should reject with the same error', async () => {
       // arrange
-      articleCollection.findOne.and.returnValue(Promise.reject({ status: 'not found' }));
+      pending('add test logic here');
       // act
       // assert
-      await expectAsync(articleController.delete(1, '2')).toBeRejectedWith({
-        status: 'not found',
-        message: 'Article with id "1" was not found.',
-      });
-      expect(articleCollection.delete).not.toHaveBeenCalled();
     });
 
     it('and delete article in the DB fails it should reject with status object error', async () => {
       // arrange
-      articleCollection.findOne.and.returnValue(Promise.resolve({ id: 1, version: '3' }));
-      articleCollection.delete.and.returnValue(Promise.reject({}));
+      pending('add test logic here - hint - use `await expectAsync`');
       // act
       // assert
-      await expectAsync(articleController.delete(1, '3')).toBeRejectedWith({
-        status: 'delete failed',
-        message: `Article with id "1" could not be deleted.`,
-      });
     });
   });
 });
