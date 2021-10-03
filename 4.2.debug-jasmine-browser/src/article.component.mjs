23323486348db1056390e2e8b9a724bafe088db7
Georgi Parlakov <gparlakov@gmail.com>
2021-10-03 18:20:43 +0300

    feat(debug-browser): init code

---
+++ 4.2.debug-jasmine-browser/src/article.component.mjs
@@ -0,0 +1,36 @@
+export class ArticleComponent {
+  articleId; // or props.articleId
+
+  article;
+  loading;
+  destroyed;
+
+  articleAPI;
+  userMessenger;
+
+  constructor(articleApi, userMessenger) {
+    this.articleAPI = articleApi;
+    this.userMessenger = userMessenger;
+  }
+
+  async afterComponentInitialize() {
+    console.log('afterComponentInitialize')
+    this.loading = true;
+    try {
+      const article = await this.articleAPI.get(this.articleId);
+      if(!this.destroyed) {
+        this.article = article;
+      }
+    } catch {
+      if(!this.destroyed) {
+        this.userMessenger.error(`Could not fetch article id: "${this.articleId}". Please try again.`);
+      }
+    }
+    this.loading = false;
+  }
+
+  beforeComponentDestroy() {
+    this.destroyed = true;
+    this.article = undefined;
+  }
+}
