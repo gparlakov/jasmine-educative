c305e4961f7badf88548c3c0c99b52946c103f17
Georgi Parlakov <gparlakov@gmail.com>
2021-10-27 06:38:52 +0300

    editing: 1.1

--- 1.1.jasmine-standalone/src/map.js
+++ 1.1.jasmine-standalone/src/map.js
@@ -1,6 +1,6 @@
 
 /**
- * Map function (A polyfill for Array.prototype.map) that takes an array and maps over its items and returns a new array
+ * Map function (A polyfill for Array.prototype.map) that takes an array and maps over its items, and returns a new array
  * @param {Array} arr the array to iterate over and project each item via the projection arg
  * @param {(x: any) => any} projection the projection function which receives an item and returns an item
  */
